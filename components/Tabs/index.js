70e8ab667d81b1dd66351b105794ad335e62073a
Tatianna <gilliamtatiannam@gmail.com>
2019-07-12 13:57:52 -0700

    Tabs debug 1

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,21 +8,22 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-
-const createTabs = document.querySelector('.tabs')
-console.log('Yooo', createTabs)
-
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-   .then((data) =>{
-    // gives results from the server
-    console.log('response', data)
-    let tabs = data.data
-    console.log('Yooo', tabs)
-    const element =createTabs(tabs)
-    console.log('Rip Nip the Great', tabs)
-    tabs.appendChild(element)
-   })
-
-   .catch((error) => {
-       console.log('Error', error);
-   })
+function createTabs(topics){ 
+    const createTabs = document.querySelector('.tabs')
+    console.log('Yooo', createTabs)
+     
+       .then((data) =>{
+        // gives results from the server
+        console.log('response', data)
+        let tabs = data.data
+        console.log('Yooo', tabs)
+        const element =createTabs(topics)
+        console.log('Rip Nip the Great', tabs)
+        tabs.appendChild(element)
+       })
+    
+       .catch((error) => {
+           console.log('Error', error);
+       })
+    
+}
